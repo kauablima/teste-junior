8dc09d2858302dca829363d6e8d025256dc92fb4
Kauã Lima <dev.kaualima@gmail.com>
2026-06-23 19:43:41 -0300

    refactor(shared): atualizando common e os campos de participants

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,6 +1,5 @@
 export { loginSchema, type LoginType } from './schema/auth';
 export { createParticipantSchema, type CreateParticipantInput, participantResponseSchema, type ParticipantResponseSchema } from './schema/participant';
 export { createTeamSchema, type CreateTeamInput, teamResponseSchema, type TeamResponseSchema } from './schema/team';
-export { paginationSchema, type PaginationInput, searchQuerySchema, type SearchQueryInput } from './schema/pagination';
+export { paginationSchema, type PaginationInput, searchQuerySchema, type SearchQueryInput } from './schema/common';
 export { normalizeCpf, isValidCpf } from './lib/cpf';
-export { isValidDate } from './lib/birthDate';
--- packages/shared/src/schema/participant.ts
+++ packages/shared/src/schema/participant.ts
@@ -1,18 +1,19 @@
 import z from "zod";
 import { isValidCpf } from "../lib/cpf";
-import { isValidDate } from "../lib/birthDate";
+
+
 
 export const createParticipantSchema = z.object({
     name: z.string().min(3, 'Nome deve ter pelo menos 3 caracteres'),
     cpf: z.string().regex(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, 'CPF deve estar no formato XXX.XXX.XXX-XX').refine(isValidCpf, 'CPF inválido'),
     email: z.string().email('E-mail está inválido'),
-    birthDate: z.string().refine(isValidDate, 'Data de nascimento inválida, deve estar no formato AAAA/MM/DD'),
+    birthDate: z.string().regex(/^\d{2}\/\d{2}\/\d{4}$/, 'Data de nascimento inválida, deve estar no formato AAAA/MM/DD'),
     phone: z.string().regex(/^\(\d{2}\)\s\d{4,5}-\d{4}$/, 'Telefone deve estar no formato (XX) XXXX-XXXX'),
     teamId: z.string(),
     observations: z.string().max(500, 'Observações devem ter no máximo 500 caracteres').optional(),
 })
 
-export type CreateParticipantInput = z.infer<typeof createParticipantSchema>    
+export type CreateParticipantInput = z.infer<typeof createParticipantSchema>
 
 export const participantResponseSchema = z.object({
     id: z.string(),
